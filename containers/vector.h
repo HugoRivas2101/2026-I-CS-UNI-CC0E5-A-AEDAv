30e96dc1be4ff5549c0807d5a3cae6b87fa13d83
hugo <hugorivas2101@gmail.com>
2026-04-14 22:37:02 -0500

    Concurrencia en ForEach y ReverseForEach

--- containers/vector.h
+++ containers/vector.h
@@ -87,14 +87,14 @@ public:
     backward_iterator rbegin() { return backward_iterator(this, m_data + m_size - 1); }
     backward_iterator rend()   { return backward_iterator(this, m_data - 1); }
     
-    // TODO: Agregar control concurrente
+    // TODO: Agregar control concurrente. DONE
     template <typename Func, typename... Args>
     void ForEach(Func func, Args &&...  args){
         scoped_lock lock(m_mtx);
         ::ForEach(begin(), end(), func, std::forward<Args>(args)... );
     }
 
-    // TODO: Agregar control concurrente
+    // TODO: Agregar control concurrente. DONE
     template <typename Func, typename... Args>
     void ReverseForEach(Func func, Args &&...  args){
         scoped_lock lock(m_mtx);
